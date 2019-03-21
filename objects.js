d616d06d056209de646456bc822d5d9ada38e437
Greg Morgan <gregm9999@gmail.com>
2019-03-21 00:56:23 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,9 @@
-let playlist = {"Yes", "Owner of a Lonely Heart"}
+let playlist = {"Yes", "Owner of a Lonely Heart"};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+}
+
+function deletePlaylist(playlist, artistName) {
+  delete playlist[artistName];
+}
