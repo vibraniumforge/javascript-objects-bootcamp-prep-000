e4ce877aceb229ec1496279cd7af8fa1428bb58b
Greg Morgan <gregm9999@gmail.com>
2019-03-21 00:57:58 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,6 +4,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
 }
 
-function deletePlaylist(playlist, artistName) {
+function removeFromPlaylist (playlist, artistName) {
   delete playlist[artistName];
 }
